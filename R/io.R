b1d5c50f64be06cffca14894c548cd37b3baf756
Willem Stolte <willem.stolte@deltares.nl>
2015-09-24 10:41:33 +0200

    Date/time format adapted
    Now converts both date and date/time formatted strings to POSIXct

--- R/io.R
+++ R/io.R
@@ -123,7 +123,11 @@ arr2df <- function(arr, locmod, submod) {
     df.mod$variable <- submod
     df.mod$time <- row.names(df.mod)
   }
+
+  ifelse(nchar(as.character(df.mod$time[1])) < 19,
+  df.mod$time  <- as.POSIXct(x=df.mod$time, format = "%Y-%m-%d"),
   df.mod$time  <- as.POSIXct(x=df.mod$time, format = "%Y-%m-%d %H:%M:%S")
+  )
   df.mod$location  <- factor(df.mod$location,levels = locmod)
   #df.mod$species   <- factor(df.mod$species, levels = submod)
   return(df.mod)
