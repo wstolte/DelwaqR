4d5ad33d3f1975e51a5303fea53d625e5e6f32ea
Willem Stolte <willem.stolte@deltares.nl>
2016-06-24 12:57:20 +0200

    Now reads scu in seconds and compensates time step for the value in scu (change made by Lilith Kramer)

--- R/io.R
+++ R/io.R
@@ -17,68 +17,76 @@
 #'   geom_line(aes(color = variable), size = 1) +
 #'   geom_point(aes(color = variable), fill = "white",  shape = 21, size = 4) +
 #'   facet_grid((. ~ location))
-his2arr <- function(filename, timestamp = T, begintime = "1900-01-01 00:00:00"){
+his2arr<- function (filename, timestamp = T, begintime = "1900-01-01 00:00:00")
+{
   require("stringr")
-
-  ## check file name
-  if (substr(filename, nchar(filename)-3, nchar(filename))!=".his"){
+  if (substr(filename, nchar(filename) - 3, nchar(filename)) !=
+      ".his") {
     stop("filename does not seem to be a <.his> file")
   }
 
-  ## Open file in binary mode
-  zz <- file(filename, "rb")
-  ## Read header lines
-  readChar(zz,40)
-  readChar(zz,40)
-  readChar(zz,40)
-  readChar(zz,4)
-  ## reads time origin from Delwaq his file
-  timeorigin <- readChar(zz,19)
-  readChar(zz,17)
-  ## Read 2 integers
-  afm <- readBin(zz,integer(),n=2)
-  ## reserve some memory
-  syname <- vector("character",afm[1])
-  idump <- vector("integer",afm[2])
-  duname <- vector("integer",afm[2])
-  ## Now a row of characters
-  for(i in 1:afm[1]){
-    syname[i] <- readChar(zz,20)
-  }
-  ## Now a few rows of integers and strings
-  for(i in 1:afm[2]){
-    idump[i] <- readBin(zz,integer(),n=1)
-    duname[i] <- readChar(zz,20)
-  }
+  filename <- "DATA/NZBLOOM.his"
 
+  zz <- file(filename, "rb")
+  readChar(zz, 40)
+  readChar(zz, 40)
+  readChar(zz, 40)
+  readChar(zz, 4)
+  timeorigin <- readChar(zz, 19)
+  readChar(zz, 7)
+  scu.prep <- readChar(zz, 8)
+  scu  <- as.numeric(scu.prep) # check for the internal timer
+  sign.scu <- sign(scu)  # sign can be +1, 0 or -1
+
+  ifelse(is.na(sign.scu), dec.sign.scu <- NA,   # NB. no error handling here yet, NA should only occur if timestamp = F and then you don't need dec.sign.scu
+         ifelse(sign.scu == 1 , dec.sign.scu <- "*",
+                ifelse(sign.scu == -1, dec.sign.scu <- "/",
+                       ifelse(sign.scu == 0, stop("The sign of your internal timer is neither negative nor positive, but 0."), stop("Check dec.sign.scu.")))))
+
+  scu.sym  <- readChar(zz, 1)
+  readChar(zz, 1)
+  afm <- readBin(zz, integer(), n = 2)
+
+  syname <- vector("character", afm[1])
+  idump <- vector("integer", afm[2])
+  duname <- vector("integer", afm[2])
+  for (i in 1:afm[1]) {
+    syname[i] <- readChar(zz, 20)
+  }
+  for (i in 1:afm[2]) {
+    idump[i] <- readBin(zz, integer(), n = 1)
+    duname[i] <- readChar(zz, 20)
+  }
   loc <- seek(zz)
   it <- -1
-  itn <- vector("integer",0)
-  tel<-0
-  ## Keep reading until we no longer have data
-  while(length(it)>0){
-    tel<-tel+1
-    it<-readBin(zz,integer(),n=1)
-    if (length(it)>0){
-      itn<-c(itn,it)
-      conc<-readBin(zz,"double",n=afm[1]*afm[2],size=4)
+  itn <- vector("integer", 0)
+  tel <- 0
+  while (length(it) > 0) {
+    tel <- tel + 1
+    it <- readBin(zz, integer(), n = 1)
+    if (length(it) > 0) {
+      itn <- c(itn, it)
+      conc <- readBin(zz, "double", n = afm[1] * afm[2],
+                      size = 4)
+      length(conc)
     }
   }
-  ## rewind
-  seek(zz, where=loc)
-  concar <- array(dim=c(length(itn),afm[2],afm[1]))
-  for(i in 1:length(itn)){
-    it <- readBin(zz,integer(),n=1)
-    concar[i,,] <- matrix(readBin(zz,"double",n=afm[1]*afm[2],size=4),nrow=afm[2],ncol=afm[1],byrow=T)
+  seek(zz, where = loc)
+  concar <- array(dim = c(length(itn), afm[2], afm[1]))
+  for (i in 1:length(itn)) {
+    it <- readBin(zz, integer(), n = 1)
+    concar[i, , ] <- matrix(readBin(zz, "double", n = afm[1] *
+                                      afm[2], size = 4), nrow = afm[2], ncol = afm[1],
+                            byrow = T)
   }
-  ## close file connection
   close(zz)
-  ## adapt date names using timeorigin in his file
-  timeorigin <- str_replace_all(timeorigin,"[.]","-")
-  ifelse(timestamp,
-         itn2 <- as.character(as.POSIXct(x=as.numeric(itn), origin = timeorigin, tz = "GMT")),
-         itn2 <- as.character(as.POSIXct(x=as.numeric(itn), origin = begintime, tz = "GMT")))
-  dimnames(concar) <- list(itn2,str_trim(duname),str_trim(syname))
+  timeorigin <- str_replace_all(timeorigin, "[.]", "-")
+
+
+  ifelse(timestamp, itn2 <- as.character(as.POSIXct(x = sapply(itn, function(x) as.numeric(eval(parse(text = paste(x, dec.sign.scu, scu))))),
+                                                    origin = timeorigin, tz = "GMT")), itn2 <- as.character(as.POSIXct(x = as.character(as.POSIXct(x = as.numeric(itn),
+                                                                                                                                                   origin = begintime, tz = "GMT")))))
+  dimnames(concar) <- list(itn2, str_trim(duname), str_trim(syname))
   return(concar)
 }
 
